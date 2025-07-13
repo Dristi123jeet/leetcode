b883898d534537d62c14bbd37896baaa678d766c
Dristi123jeet <154901301+Dristi123jeet@users.noreply.github.com>
2025-07-13 13:13:06 +0530

    Time: 2 ms (41.56%), Space: 12.8 MB (27.93%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& arr) {
-        int n = arr.size();
-        int totalSubsets = (1 << n); // 2^n subsets
-        vector<vector<int>> subs; // Store all subsets
-        
-        for (int num = 0; num < totalSubsets; num++) {
-            vector<int> subset; // Store current subset
-            for (int j = 0; j < n; j++) {
-                if (num & (1 << j)) subset.push_back(arr[j]); // Pick elements based on bits
-            }
-            subs.push_back(subset); // Add current subset to result
+    void helper(int i, vector<int>& arr, vector<int>& curr, vector<vector<int>>& ans) {
+        if (i == arr.size()) {
+            ans.push_back(curr);
+            return;
         }
-        return subs;
+
+        // Exclude current element
+        helper(i + 1, arr, curr, ans);
+
+        // Include current element
+        curr.push_back(arr[i]);
+        helper(i + 1, arr, curr, ans);
+        curr.pop_back(); // backtrack
+    }
+
+    vector<vector<int>> subsets(vector<int>& arr) {
+        vector<vector<int>> anss;
+        vector<int> curr;
+        helper(0, arr, curr, anss);
+        return anss;
     }
 };
