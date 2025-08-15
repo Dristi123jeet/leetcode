cfcccc261c9fb160da62f9c76827bde870bbd761
Dristi123jeet <154901301+Dristi123jeet@users.noreply.github.com>
2025-08-15 18:28:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    private:
+    int helper(int i, vector<int>nums, vector<int>dp){
+        if(i==0)return nums[0];
+        if(i<1)return 0;
+        if(dp[i]!=-1)return dp[i];
+        int p= nums[i]+ helper(i-2, nums, dp);
+        int np= 0+ helper(i-1, nums, dp);
+        return dp[i]= max(p,np);
+    }
+public:
+    int rob(vector<int>& nums) {
+        int n= nums.size();
+        vector<int>dp(n,-1);
+         return helper(n-1, nums, dp);
+    }
+};
